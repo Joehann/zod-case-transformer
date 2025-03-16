7940554fb673250a165beae2e16c2a8dc298d32f
Joehann <jpouponnot@outlook.fr>
2025-03-16 14:44:13 +0100

    Refactor and export camelizeSchema as default
    Changed camelizeSchema from an exported constant to a default export to improve module usability. Adjusted related code to align with this modification.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import {z} from "zod";
 import DeepSnakeToCamel from "./transformers/deep-snake-to-camel.type";
 import {deepSnakeToCamel} from "./transformers/deep-snake-to-camel";
 
-export const camelizeSchema = <T extends z.ZodTypeAny>(schema: T) => {
+const camelizeSchema = <T extends z.ZodTypeAny>(schema: T) => {
     return schema.transform(
         (data: z.infer<T>): DeepSnakeToCamel<z.infer<T>> =>
             deepSnakeToCamel(data)
     )
 }
+
+export default camelizeSchema;
